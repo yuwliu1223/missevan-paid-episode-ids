b3669fcdd8ba51bae90e7af9190fa46c46e595d9
Momo <liuyuwei1223@gmail.com>
2024-06-14 16:32:02 -0400

    Update maoer_csv.py

--- maoer_csv.py
+++ maoer_csv.py
@@ -1,6 +1,7 @@
 import csv
 import datetime
 import logging
+import time
 import xml.etree.ElementTree as ETree
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import requests
@@ -9,12 +10,15 @@ import requests
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 BASE_URL = "https://www.missevan.com"
-
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9'
+}
 
 def get_drama_sound_lists(drama_id):
     url = f"{BASE_URL}/dramaapi/getdrama?drama_id={drama_id}"
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         response.raise_for_status()
         data = response.json().get("info", {})
         drama = data.get('drama', {})
@@ -35,7 +39,7 @@ def get_drama_sound_lists(drama_id):
 def get_sound_detail(sound_id):
     url = f"{BASE_URL}/sound/getsound?soundid={sound_id}"
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         response.raise_for_status()
         sound = response.json().get("info", {}).get("sound", {})
 
@@ -46,7 +50,7 @@ def get_sound_detail(sound_id):
             "comment_count": sound.get("comment_count"),
             "favorite_count": sound.get("favorite_count"),
             "username": sound.get("username"),
-            "create_time": datetime.datetime.fromtimestamp(sound.get('create_time', 0)),
+            "create_time": datetime.datetime.fromtimestamp(sound.get('create_time', 0)) if sound.get('create_time', 0) > 0 else None,
         }
     except requests.RequestException as e:
         logging.error(f"Error fetching sound detail for sound ID {sound_id}: {e}")
@@ -56,7 +60,7 @@ def get_sound_detail(sound_id):
 def fetch_all_danmakus(sound_id):
     url = f"{BASE_URL}/sound/getdm?soundid={sound_id}"
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         response.raise_for_status()
         pp_comments_xml = ETree.fromstring(response.text)
         return {int(item.attrib["p"].split(",")[6]) for item in pp_comments_xml.findall("d") if
@@ -79,7 +83,7 @@ def fetch_all_uids_by_comments(sound_id):
     page = 1
 
     while True:
-        response = requests.get(endpoint.format(page))
+        response = requests.get(endpoint.format(page), headers=headers)
         response.raise_for_status()
         data = response.json()
         comments_uids.update(extract_user_ids(data))
@@ -113,6 +117,70 @@ def process_sound(sound):
     return sound_detail
 
 
+# def process_drama_id(drama_id, sound_writer, drama_writer):
+#     logging.info(f"Processing drama: (ID: {drama_id})")
+#     sound_lists, name, price, view_count, catalog_name = get_drama_sound_lists(drama_id)
+#     sound_data = []
+#     total_paid_udis = set()
+#     total_free_udis = set()
+#
+#     total_paid_danmaku_udis = set()
+#     total_paid_comment_uids = set()
+#     total_free_danmaku_udis = set()
+#     total_free_comment_uids = set()
+#
+#     paid_view_count = 0
+#     free_view_count = 0
+#     first_sound_create_time = None
+#
+#     if sound_lists:
+#         with ThreadPoolExecutor() as executor:
+#             future_to_sound = {executor.submit(process_sound, sound): sound for sound in sound_lists}
+#             for future in as_completed(future_to_sound):
+#                 sound_detail = future.result()
+#                 if first_sound_create_time is None or sound_detail['create_time'] < first_sound_create_time:
+#                     first_sound_create_time = sound_detail['create_time']
+#                 if sound_detail:
+#                     if future_to_sound[future].get('need_pay') > 0:
+#                         total_paid_udis.update(sound_detail['total_sound_uids'])
+#                         paid_view_count += int(sound_detail['view_count'])
+#
+#                         total_paid_danmaku_udis.update(sound_detail['danmaku_uids'])
+#                         total_paid_comment_uids.update(sound_detail['comment_uids'])
+#                     else:
+#                         total_free_udis.update(sound_detail['total_sound_uids'])
+#                         total_free_danmaku_udis.update(sound_detail['danmaku_uids'])
+#                         total_free_comment_uids.update(sound_detail['comment_uids'])
+#                         free_view_count += int(sound_detail['view_count'])
+#
+#                     sound_data.append(sound_detail)
+#                     print(sound_detail['sound_title'], sound_detail['create_time'], sound_detail['need_pay'],
+#                           len(sound_detail['danmaku_uids']), len(sound_detail['comment_uids']),
+#                           len(sound_detail['total_sound_uids']), sound_detail['view_count'])
+#
+#
+#     # Order sound_data by sound_id
+#     sound_data.sort(key=lambda x: x['sound_id'])
+#
+#     for sound_detail in sound_data:
+#         sound_writer.writerow([
+#             sound_detail['sound_title'], sound_detail['create_time'], ('PAID' if int(sound_detail['need_pay']) > 0 else 'FREE'),
+#             len(sound_detail['danmaku_uids']), len(sound_detail['comment_uids']),
+#             len(sound_detail['total_sound_uids']), sound_detail['view_count']
+#         ])
+#
+#     # Add two new rows after the sound data
+#     sound_writer.writerow(['End of data for drama ID', drama_id, '', '', '', '', ''])
+#     sound_writer.writerow(['', '', '', '', '', '', ''])
+#     sound_writer.writerow(['', '', '', '', '', '', ''])
+#
+#     drama_writer.writerow([
+#         drama_id, name, first_sound_create_time, price, view_count, paid_view_count, free_view_count,
+#         len(total_paid_danmaku_udis), len(total_paid_comment_uids), len(total_free_danmaku_udis),
+#         len(total_free_comment_uids), len(total_paid_udis), len(total_free_udis)
+#     ])
+#
+#     return sound_data, total_paid_udis
 def process_drama_id(drama_id, sound_writer, drama_writer):
     logging.info(f"Processing drama: (ID: {drama_id})")
     sound_lists, name, price, view_count, catalog_name = get_drama_sound_lists(drama_id)
@@ -130,30 +198,27 @@ def process_drama_id(drama_id, sound_writer, drama_writer):
     first_sound_create_time = None
 
     if sound_lists:
-        with ThreadPoolExecutor() as executor:
-            future_to_sound = {executor.submit(process_sound, sound): sound for sound in sound_lists}
-            for future in as_completed(future_to_sound):
-                sound_detail = future.result()
-                if first_sound_create_time is None or sound_detail['create_time'] < first_sound_create_time:
-                    first_sound_create_time = sound_detail['create_time']
-                if sound_detail:
-                    if future_to_sound[future].get('need_pay') > 0:
-                        total_paid_udis.update(sound_detail['total_sound_uids'])
-                        paid_view_count += int(sound_detail['view_count'])
+        for sound in sound_lists:
+            sound_detail = process_sound(sound)
+            if first_sound_create_time is None or sound_detail['create_time'] < first_sound_create_time:
+                first_sound_create_time = sound_detail['create_time']
+            if sound_detail:
+                if sound.get('need_pay') > 0:
+                    total_paid_udis.update(sound_detail['total_sound_uids'])
+                    paid_view_count += int(sound_detail['view_count'])
 
-                        total_paid_danmaku_udis.update(sound_detail['danmaku_uids'])
-                        total_paid_comment_uids.update(sound_detail['comment_uids'])
-                    else:
-                        total_free_udis.update(sound_detail['total_sound_uids'])
-                        total_free_danmaku_udis.update(sound_detail['danmaku_uids'])
-                        total_free_comment_uids.update(sound_detail['comment_uids'])
-                        free_view_count += int(sound_detail['view_count'])
+                    total_paid_danmaku_udis.update(sound_detail['danmaku_uids'])
+                    total_paid_comment_uids.update(sound_detail['comment_uids'])
+                else:
+                    total_free_udis.update(sound_detail['total_sound_uids'])
+                    total_free_danmaku_udis.update(sound_detail['danmaku_uids'])
+                    total_free_comment_uids.update(sound_detail['comment_uids'])
+                    free_view_count += int(sound_detail['view_count'])
 
-                    sound_data.append(sound_detail)
-                    print(sound_detail['sound_title'], sound_detail['create_time'], sound_detail['need_pay'],
-                          len(sound_detail['danmaku_uids']), len(sound_detail['comment_uids']),
-                          len(sound_detail['total_sound_uids']), sound_detail['view_count'])
-
+                sound_data.append(sound_detail)
+                print(sound_detail['sound_title'], sound_detail['create_time'], sound_detail['need_pay'],
+                      len(sound_detail['danmaku_uids']), len(sound_detail['comment_uids']),
+                      len(sound_detail['total_sound_uids']), sound_detail['view_count'])
 
     # Order sound_data by sound_id
     sound_data.sort(key=lambda x: x['sound_id'])
@@ -164,6 +229,11 @@ def process_drama_id(drama_id, sound_writer, drama_writer):
             len(sound_detail['danmaku_uids']), len(sound_detail['comment_uids']),
             len(sound_detail['total_sound_uids']), sound_detail['view_count']
         ])
+
+    # Add two new rows after the sound data
+    sound_writer.writerow(['End of data for drama ID', drama_id, '', '', '', '', ''])
+    sound_writer.writerow(['', '', '', '', '', '', ''])
+    sound_writer.writerow(['', '', '', '', '', '', ''])
 
     drama_writer.writerow([
         drama_id, name, first_sound_create_time, price, view_count, paid_view_count, free_view_count,
@@ -179,21 +249,29 @@ def runner():
     drama_sound = {}
     all_paid_total_uids = set()
 
-    with open('sound_data.csv', mode='w', newline='', encoding='utf-8') as sound_file, \
-            open('drama_data.csv', mode='w', newline='', encoding='utf-8') as drama_file:
+    with open('sound_data.csv', mode='a', newline='', encoding='utf-8') as sound_file, \
+            open('drama_data.csv', mode='a', newline='', encoding='utf-8') as drama_file:
         sound_writer = csv.writer(sound_file)
         drama_writer = csv.writer(drama_file)
 
-        sound_writer.writerow(["声音标题", "创建时间", "是否需要付费", "弹幕用户ID", "评论用户ID", "总用户ID", "观看次数"])
-        drama_writer.writerow(
-            ["剧集ID", "剧集名称", "首个声音创建时间", "价格", "总观看次数", "付费观看次数", "免费观看次数",
-             "付费弹幕用户ID", "付费评论用户ID", "免费弹幕用户ID", "免费评论用户ID",
-             "付费总用户ID", "免费总用户ID"])
+        # Check if the file is empty before writing headers
+        sound_file_empty = sound_file.tell() == 0
+        drama_file_empty = drama_file.tell() == 0
+
+        if sound_file_empty:
+            sound_writer.writerow(["声音标题", "创建时间", "是否需要付费", "弹幕用户ID", "评论用户ID", "总用户ID", "观看次数"])
+
+        if drama_file_empty:
+            drama_writer.writerow(
+                ["剧集ID", "剧集名称", "首个声音创建时间", "价格", "总观看次数", "付费观看次数", "免费观看次数",
+                 "付费弹幕用户ID", "付费评论用户ID", "免费弹幕用户ID", "免费评论用户ID",
+                 "付费总用户ID", "免费总用户ID"])
 
         for drama_id in drama_ids.split(','):
             sound_data, total_paid_udis = process_drama_id(drama_id.strip(), sound_writer, drama_writer)
             drama_sound[drama_id] = sound_data
             all_paid_total_uids.update(total_paid_udis)
+            time.sleep(60)
 
     print('-------------------------------------------------')
     print(f"All Paid Total UIDs: {len(all_paid_total_uids)}")
